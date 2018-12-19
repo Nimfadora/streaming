cea49a5d52d52ed33819a1be0716c17dd30734bc
Valeriia Vasileva <valeriia_vasileva@epam.com>
2018-12-19 16:28:51 +0300

    add streaming consumer

---
+++ streaming/src/main/scala/com/vasileva/streaming/consumer/StreamingConsumerMain.scala
@@ -0,0 +1,30 @@
+package com.vasileva.streaming.consumer
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.OutputMode
+
+object StreamingConsumerMain {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder
+      .appName("StreamingConsumer")
+      .getOrCreate()
+
+    val query = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
+      .option("subscribe", args(0))
+      .load()
+      .selectExpr("CAST(value AS STRING)")
+      .writeStream
+      .format("csv")
+      .outputMode(OutputMode.Append())
+      .option("checkpointLocation", "/user/root/tmp/spark-streaming/checkpoint")
+      .option("path", args(1))
+      .option("quote", "")
+      .start()
+    query.awaitTermination()
+  }
+}
