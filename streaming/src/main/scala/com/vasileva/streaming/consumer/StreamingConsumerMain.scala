c7c8112afa5239e22fc809e07c6f8392c50bfcc1
Valeriia Vasileva <valeriia_vasileva@epam.com>
2018-12-21 15:06:13 +0300

    change starting offset for streaming consumer to earliest, add autostop when there is no data left to consume, add javadoc

--- streaming/src/main/scala/com/vasileva/streaming/consumer/StreamingConsumerMain.scala
+++ streaming/src/main/scala/com/vasileva/streaming/consumer/StreamingConsumerMain.scala
@@ -1,8 +1,11 @@
 package com.vasileva.streaming.consumer
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 
+/**
+  * Streams data from Kafka and saves it to HDFS
+  */
 object StreamingConsumerMain {
   def main(args: Array[String]): Unit = {
 
@@ -16,15 +19,17 @@ object StreamingConsumerMain {
       .format("kafka")
       .option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
       .option("subscribe", args(0))
+      .option("startingOffsets", "earliest")
       .load()
       .selectExpr("CAST(value AS STRING)")
       .writeStream
+      .trigger(Trigger.ProcessingTime("2 seconds"))
       .format("csv")
       .outputMode(OutputMode.Append())
       .option("checkpointLocation", "/user/root/tmp/spark-streaming/checkpoint")
       .option("path", args(1))
       .option("quote", "")
       .start()
-    query.awaitTermination()
+    query.processAllAvailable()
   }
 }
